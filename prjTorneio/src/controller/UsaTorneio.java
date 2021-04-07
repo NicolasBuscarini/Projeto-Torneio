de48843e1aa65c2c76070543796f29536f5a8567
Nicolas Buscarini <78824150+NicolasBuscarini@users.noreply.github.com>
2021-04-07 18:19:38 -0300

    projeto-torneio

---
+++ prjTorneio/src/controller/UsaTorneio.java
@@ -0,0 +1,12 @@
+package controller;
+
+import model.Torneio;
+
+
+public class UsaTorneio {
+    public static void main(String[] args) {
+        Torneio t1 = new Torneio("Nicolas", 4);
+        t1.ImprimirDados();
+    }
+    
+}
